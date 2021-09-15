7ad6b96f320d6cb8a131b8b8ffbf1487d7ea70e2
Bastian Neumann <bneumann@kurzschluss-engineering.de>
2021-09-15 23:29:24 +0200

    Memory allocation checks for availability and prints info if not available

--- lib/Platinenmacher/memory.h
+++ lib/Platinenmacher/memory.h
@@ -15,20 +15,21 @@
 
 #include <string.h>
 
-inline void *RTOS_Malloc(size_t size)
+inline static void *RTOS_Malloc(size_t size)
 {
     void *mem = malloc(size);
-    memset(mem, 0, size);
+    if(mem)
+        memset(mem, 0, size);
+    else
+        ESP_LOGI("MALLOC", "failed to allocate: %d of %d", size, heap_caps_get_free_size(MALLOC_CAP_8BIT));
     return mem;
 }
-inline void RTOS_Free(void *pointer)
+inline static void RTOS_Free(void *pointer)
 {
-    ESP_LOGI("MALLOC", "free: %d", xPortGetFreeHeapSize());
     free(pointer);
 }
 
 #define bit_set(data, pos) (data |= (1U << pos))
 #define bit_clear(data, pos) (data &= (~(1U << pos)))
 #define bit_toggle(data, pos) (data ^= (1U << pos))
-
 #endif /* PLATINENMACHER_MEMORY_H_ */
