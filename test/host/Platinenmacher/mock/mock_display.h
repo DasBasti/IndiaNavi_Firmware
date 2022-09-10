9d8cd98063b6360b93e5140c2b52022b3c4e12cf
Bastian Neumann <neumann.bastian@gmail.com>
2022-09-10 11:48:31 +0200

    Testing: move screen size to mock file

--- test/host/Platinenmacher/mock/mock_display.h
+++ test/host/Platinenmacher/mock/mock_display.h
@@ -11,17 +11,19 @@
 
 #include "display.h"
 
-error_code_t write_pixel(struct display *dsp, uint16_t x, uint16_t y,
-                         uint8_t color)
+error_code_t write_pixel(struct display* dsp, uint16_t x, uint16_t y,
+    uint8_t color)
 {
     dsp->fb[((y * dsp->size.width) + x)] = color;
     return PM_OK;
 }
 
-uint8_t decompress(rect_t *size, uint16_t x, uint16_t y, uint8_t *data)
+uint8_t decompress(rect_t* size, uint16_t x, uint16_t y, uint8_t* data)
 {
 
-    return data[x*size->width+y];
+    return data[x * size->width + y];
 }
 
+#define DISPLAY_WIDTH 20
+#define DISPLAY_HEIGHT 20
 #endif /* TEST_HOST_PLATINENMACHER_MOCK_DISPLAY_H */
