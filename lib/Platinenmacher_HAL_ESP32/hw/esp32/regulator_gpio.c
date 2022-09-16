986f30b5b1b6df73e10678a91b492c3c7915a269
Bastian Neumann <neumann.bastian@gmail.com>
2022-09-16 23:11:04 +0200

    Remove warnings: unused variable in gpio module
    This is part of #6

--- lib/Platinenmacher_HAL_ESP32/hw/esp32/regulator_gpio.c
+++ lib/Platinenmacher_HAL_ESP32/hw/esp32/regulator_gpio.c
@@ -9,44 +9,38 @@
 #include "hw/gpio.h"
 #include "memory.h"
 
-void regulator_gpio_enable(regulator_t *reg)
+void regulator_gpio_enable(regulator_t* reg)
 {
-	gpio_t *gpio = (gpio_t *)reg->driver;
-	if (reg->driver)
-	{
-		reg->usage++;
-		if (reg->status != ON)
-		{
-			gpio_t *gpio = (gpio_t *)reg->driver;
-			gpio_write(gpio, gpio->onValue);
-			reg->status = ON;
-		}
-	}
+    if (reg->driver) {
+        gpio_t* gpio = (gpio_t*)reg->driver;
+        reg->usage++;
+        if (reg->status != ON) {
+            gpio_write(gpio, gpio->onValue);
+            reg->status = ON;
+        }
+    }
 }
 
-void regulator_gpio_disable(regulator_t *reg)
+void regulator_gpio_disable(regulator_t* reg)
 {
-	if (reg->driver)
-	{
-		gpio_t *gpio = (gpio_t *)reg->driver;
-		if (reg->status != OFF)
-		{
-			reg->usage--;
-		}
-		if (reg->usage == 0)
-		{
-			gpio_write(gpio, !gpio->onValue);
-			reg->status = OFF;
-		}
-	}
+    if (reg->driver) {
+        gpio_t* gpio = (gpio_t*)reg->driver;
+        if (reg->status != OFF) {
+            reg->usage--;
+        }
+        if (reg->usage == 0) {
+            gpio_write(gpio, !gpio->onValue);
+            reg->status = OFF;
+        }
+    }
 }
 
-regulator_t *regulator_gpio_create(gpio_t *gpio)
+regulator_t* regulator_gpio_create(gpio_t* gpio)
 {
-	regulator_t *reg = RTOS_Malloc(sizeof(regulator_t));
-	reg->driver = gpio;
-	reg->enable = regulator_gpio_enable;
-	reg->disable = regulator_gpio_disable;
-	reg->disable(reg);
-	return reg;
+    regulator_t* reg = RTOS_Malloc(sizeof(regulator_t));
+    reg->driver = gpio;
+    reg->enable = regulator_gpio_enable;
+    reg->disable = regulator_gpio_disable;
+    reg->disable(reg);
+    return reg;
 }
