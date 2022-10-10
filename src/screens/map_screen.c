bebe017f9460b663c68a48468e9d5922e8773925
Bastian Neumann <neumann.bastian@gmail.com>
2022-10-10 22:46:16 +0200

    Map Screen: Map now shows a height graph
    The graph is generated from trkpt data in track.gpx

--- src/screens/map_screen.c
+++ src/screens/map_screen.c
@@ -6,6 +6,7 @@
  * SPDX-License-Identifier: MIT
  */
 
+#include "gui/graph.h"
 #include "gui/label.h"
 #include "gui/map.h"
 
@@ -30,8 +31,11 @@ static uint8_t zoom_level_selected = 0;
 uint8_t zoom_level[] = { 16, 14 };
 uint8_t zoom_level_scaleBox_width[] = { 63, 77 };
 char* zoom_level_scaleBox_text[] = { "100m", "500m" };
+float* height_graph_data;
+uint16_t height_graph_data_len = 0;
+float height_min = __FLT_MAX__, height_max = 0;
 
-static const char* TAG="map_screen";
+static const char* TAG = "map_screen";
 
 /**
  * render cb for InfoText label
@@ -83,34 +87,43 @@ error_code_t updateSatsInView(const display_t* dsp, void* comp)
     return PM_OK;
 }
 
-void add_waypoints_to_renderer(waypoint_t *wp)
+void add_waypoints_to_renderer(waypoint_t* wp)
 {
-	if(wp->active)
-		add_to_render_pipeline(waypoint_render_marker, wp, RL_PATH);
+    if (wp->active)
+        add_to_render_pipeline(waypoint_render_marker, wp, RL_PATH);
 }
 
 static error_code_t map_pre_render_cb(const display_t* dsp, void* component)
 {
     // Only modify map if we are GPS fixed
-    if (!map_position || map_position->fix == GPS_FIX_INVALID){
+    if (!map_position || map_position->fix == GPS_FIX_INVALID) {
         return NOT_NEEDED;
-	}
+    }
 
-	if(gps_indicator_label){
-		gps_indicator_label->onBeforeRender = updateSatsInView;
-	}
+    if (gps_indicator_label) {
+        gps_indicator_label->onBeforeRender = updateSatsInView;
+    }
     map_update_position(map, map_position);
-	map_update_waypoint_path(map);
-	
-	free_render_pipeline(RL_PATH);
-	map_run_on_waypoints(add_waypoints_to_renderer);
+    map_update_waypoint_path(map);
+
+    free_render_pipeline(RL_PATH);
+    map_run_on_waypoints(add_waypoints_to_renderer);
 
     return PM_OK;
 }
 
+void populate_height_data(waypoint_t* wp)
+{
+    height_graph_data[wp->num] = wp->ele;
+    if (wp->ele < height_min)
+        height_min = wp->ele;
+    if (wp->ele > height_max)
+        height_max = wp->ele;
+}
+
 void load_waypoint_file(char* filename)
 {
-	uint64_t start = esp_timer_get_time();
+    uint64_t start = esp_timer_get_time();
 
     async_file_t wp_file;
     wp_file.filename = filename;
@@ -118,11 +131,15 @@ void load_waypoint_file(char* filename)
     createFileBuffer(&wp_file);
     loadFile(&wp_file);
 
-    waypoint_t *first_wp = gpx_parser(wp_file.dest, map_add_waypoint);
+    waypoint_t* first_wp = gpx_parser(wp_file.dest, map_add_waypoint, &height_graph_data_len);
     map_set_first_waypoint(first_wp);
     RTOS_Free(wp_file.dest);
 
-	ESP_LOGI(TAG, "Load waypoint information done. Took: %d ms", (uint32_t)(esp_timer_get_time() - start) / 1000);
+    // populate height data
+    height_graph_data = RTOS_Malloc(sizeof(float) * height_graph_data_len+1);
+    map_run_on_waypoints(populate_height_data);
+
+    ESP_LOGI(TAG, "Load waypoint information done. Took: %d ms", (uint32_t)(esp_timer_get_time() - start) / 1000);
     ESP_LOGI(TAG, "Heap Free: %d Byte", xPortGetFreeHeapSize());
 }
 
@@ -145,36 +162,44 @@ void map_screen_create(const display_t* display)
 
     /* scale 63px for 100m | 96px for 500ft @ zoom 16*/
     /* scale 77px for 500m | 94px for 2000ft @zoom 14 */
-    scaleBox = label_create("100m", &f8x8, 10, dsp->size.height - 34, 63, 13);
+    scaleBox = label_create("100m", &f8x8, 10, dsp->size.height - 15 - 45, 63, 13);
     scaleBox->borderWidth = 1;
     scaleBox->borderLines = LEFT_SOLID | RIGHT_SOLID | BOTTOM_SOLID;
-    //scaleBox->backgroundColor = WHITE;
+    // scaleBox->backgroundColor = WHITE;
     scaleBox->borderColor = BLACK;
     scaleBox->textColor = BLACK;
     scaleBox->alignVertical = BOTTOM;
     scaleBox->alignHorizontal = CENTER;
     add_to_render_pipeline(label_render, scaleBox, RL_TOP);
 
-    map_copyright = label_create("(c) OpenStreetMap contributors", &f8x8, 0, dsp->size.height - 22, 0, 0);
+    map_copyright = label_create("(c) OpenStreetMap contributors", &f8x8, 0, dsp->size.height - 10 - 45, 0, 0);
     label_shrink_to_text(map_copyright);
     map_copyright->box.left = dsp->size.width - map_copyright->box.width;
     add_to_render_pipeline(label_render, map_copyright, RL_GUI_ELEMENTS);
-
-    infoBox = label_create("", &f8x8, 0, dsp->size.height - 14,
-        dsp->size.width - 1, 13);
-    infoBox->borderWidth = 1;
-    infoBox->borderLines = ALL_SOLID;
-    infoBox->alignVertical = MIDDLE;
-    infoBox->backgroundColor = WHITE;
-    infoBox->onBeforeRender = updateInfoText;
-    add_to_render_pipeline(label_render, infoBox, RL_GUI_ELEMENTS);
-
+    /*
+        infoBox = label_create("", &f8x8, 0, dsp->size.height - 14,
+            dsp->size.width - 1, 13);
+        infoBox->borderWidth = 1;
+        infoBox->borderLines = ALL_SOLID;
+        infoBox->alignVertical = MIDDLE;
+        infoBox->backgroundColor = WHITE;
+        infoBox->onBeforeRender = updateInfoText;
+        add_to_render_pipeline(label_render, infoBox, RL_GUI_ELEMENTS);
+    */
     map_update_zoom_level(map, zoom_level[0]);
-    //map_update_position(map, map_position);
+    // map_update_position(map, map_position);
     map_tile_attach_onBeforeRender_callback(map, load_map_tile_on_demand);
     map_tile_attach_onAfterRender_callback(map, check_if_map_tile_is_loaded);
 
-	load_waypoint_file("//track.gpx");
+    load_waypoint_file("//track.gpx");
+
+    graph_t* graph = graph_create(0, display->size.height - 45, display->size.width, 45, height_graph_data, height_graph_data_len, &f8x8);
+    graph_set_range(graph, height_min, height_max);
+    graph->current_position = 4;
+    graph->current_position_color = BLUE;
+    graph->line_color = BLACK;
+
+    add_to_render_pipeline(graph_renderer, graph, RL_GUI_ELEMENTS);
 }
 
 void toggleZoom()
@@ -182,11 +207,11 @@ void toggleZoom()
     zoom_level_selected = !zoom_level_selected;
     map_update_zoom_level(map, zoom_level[zoom_level_selected]);
 
-    //ESP_LOGI(TAG, "Set zoom level to: %d", tile_zoom);
-    // TODO: Update Waypoints
+    // ESP_LOGI(TAG, "Set zoom level to: %d", tile_zoom);
+    //  TODO: Update Waypoints
     scaleBox->box.width = zoom_level_scaleBox_width[zoom_level_selected];
     scaleBox->text = zoom_level_scaleBox_text[zoom_level_selected];
 
-    //map_update_position(map, map_position);
+    // map_update_position(map, map_position);
     trigger_rendering();
 }
