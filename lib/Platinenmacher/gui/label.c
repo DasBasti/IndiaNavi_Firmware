ad0b05768d297ba4a590bb7827f686d5a7b663f3
Bastian Neumann <neumann.bastian@gmail.com>
2022-10-10 23:13:01 +0200

    Label: fix border width issue #23

--- lib/Platinenmacher/gui/label.c
+++ lib/Platinenmacher/gui/label.c
@@ -1,195 +1,184 @@
 /*
- * label.c
+ * Map component for loading tile images according to a position
  *
- *  Created on: 04.01.2021
- *      Author: bastian
+ * Copyright (c) 2022, Bastian Neumann <info@platinenmacher.tech>
+ *
+ * SPDX-License-Identifier: MIT
  */
 
 #include "label.h"
-#include "image.h"
 #include "display.h"
+#include "image.h"
 
 /*
  * Create a label and returns a pointer to the label_t
  */
-label_t *label_create(char *text, font_t *font, int16_t left, int16_t top,
-					  uint16_t width, uint16_t height)
+label_t* label_create(char* text, font_t* font, int16_t left, int16_t top,
+    uint16_t width, uint16_t height)
 {
-	label_t *label = RTOS_Malloc(sizeof(label_t));
-	label->onBeforeRender = NULL;
-	label->onAfterRender = NULL;
-	label->text = text;
-	label->font = font;
-	label->box.left = left;
-	label->box.top = top;
-	label->box.width = width;
-	label->box.height = height;
-	label->backgroundColor = TRANSPARENT;
-	label->textColor = BLACK;
-	label->borderColor = BLACK;
-	label->borderWidth = 0;
-	label->borderLines = NO_BORDER;
-	label->textPosition.left = 0;
-	label->textPosition.top = 0;
-	label->roundedCorners = 0;
-	label->roundedRadius = 0;
+    label_t* label = RTOS_Malloc(sizeof(label_t));
+    label->onBeforeRender = NULL;
+    label->onAfterRender = NULL;
+    label->text = text;
+    label->font = font;
+    label->box.left = left;
+    label->box.top = top;
+    label->box.width = width;
+    label->box.height = height;
+    label->backgroundColor = TRANSPARENT;
+    label->textColor = BLACK;
+    label->borderColor = BLACK;
+    label->borderWidth = 0;
+    label->borderLines = NO_BORDER;
+    label->textPosition.left = 0;
+    label->textPosition.top = 0;
+    label->roundedCorners = 0;
+    label->roundedRadius = 0;
 
-	return label;
+    return label;
 }
 
-error_code_t label_render(const display_t *dsp, void *component)
+error_code_t label_render(const display_t* dsp, void* component)
 {
-	label_t *label = (label_t *)component;
-	if (label->onBeforeRender)
-		if (label->onBeforeRender(dsp, label) != PM_OK)
-		{
-			return ABORT;
-		}
+    label_t* label = (label_t*)component;
+    if (label->onBeforeRender)
+        if (label->onBeforeRender(dsp, label) != PM_OK) {
+            return ABORT;
+        }
 
-	if (label->backgroundColor != TRANSPARENT)
-		display_rect_fill(dsp, label->box.left, label->box.top,
-						  label->box.width, label->box.height, label->backgroundColor);
+    if (label->backgroundColor != TRANSPARENT)
+        display_rect_fill(dsp, label->box.left, label->box.top,
+            label->box.width, label->box.height, label->backgroundColor);
 
-	if (label->alignHorizontal == LEFT)
-		label->textPosition.left = 1;
-	if (label->alignHorizontal == CENTER)
-	{
-		label->textPosition.left = 1 + (label->box.width - font_text_pixel_width(label->font, label->text)) / 2;
-	}
-	if (label->alignHorizontal == RIGHT)
-	{
-		label->textPosition.left = label->box.width - font_text_pixel_width(label->font, label->text) - 1;
-	}
+    if (label->alignHorizontal == LEFT)
+        label->textPosition.left = 1;
+    if (label->alignHorizontal == CENTER) {
+        label->textPosition.left = 1 + (label->box.width - font_text_pixel_width(label->font, label->text)) / 2;
+    }
+    if (label->alignHorizontal == RIGHT) {
+        label->textPosition.left = label->box.width - font_text_pixel_width(label->font, label->text) - 1;
+    }
 
-	if (label->alignVertical == TOP)
-		label->textPosition.top = 1;
-	if (label->alignVertical == MIDDLE)
-	{
-		label->textPosition.top = 1 + (label->box.height - font_text_pixel_height(label->font, label->text)) / 2;
-	}
-	if (label->alignVertical == BOTTOM)
-	{
-		label->textPosition.top = (label->box.height - font_text_pixel_height(label->font, label->text)) - 1;
-	}
+    if (label->alignVertical == TOP)
+        label->textPosition.top = 1;
+    if (label->alignVertical == MIDDLE) {
+        label->textPosition.top = 1 + (label->box.height - font_text_pixel_height(label->font, label->text)) / 2;
+    }
+    if (label->alignVertical == BOTTOM) {
+        label->textPosition.top = (label->box.height - font_text_pixel_height(label->font, label->text)) - 1;
+    }
 
-	if (font_strlen(label->text))
-		display_text_draw(dsp, label->font,
-						  label->box.left + label->textPosition.left,
-						  label->box.top + label->textPosition.top, label->text,
-						  label->textColor);
+    if (font_strlen(label->text))
+        display_text_draw(dsp, label->font,
+            label->box.left + label->textPosition.left,
+            label->box.top + label->textPosition.top, label->text,
+            label->textColor);
 
-	if (label->borderWidth)
-	{
-		/* Draw all borders */
-		if (label->roundedCorners == 0)
-		{
-			if (label->borderLines & TOP_SOLID)
-				display_line_draw(dsp, label->box.left, label->box.top,
-								  label->box.left + label->box.width, label->box.top,
-								  label->borderColor);
+    if (label->borderWidth) {
+        uint16_t width = label->box.width - 1;
+        uint16_t height = label->box.height - 1;
+        /* Draw all borders */
+        if (label->roundedCorners == 0) {
+            if (label->borderLines & TOP_SOLID)
+                display_line_draw(dsp, label->box.left, label->box.top,
+                    label->box.left + width, label->box.top,
+                    label->borderColor);
 
-			if (label->borderLines & BOTTOM_SOLID)
-				display_line_draw(dsp, label->box.left, label->box.top + label->box.height,
-								  label->box.left + label->box.width,
-								  label->box.top + label->box.height, label->borderColor);
+            if (label->borderLines & BOTTOM_SOLID)
+                display_line_draw(dsp, label->box.left, label->box.top + height,
+                    label->box.left + width,
+                    label->box.top + height, label->borderColor);
 
-			if (label->borderLines & LEFT_SOLID)
-				display_line_draw(dsp, label->box.left, label->box.top,
-								  label->box.left,
-								  label->box.top + label->box.height, label->borderColor);
+            if (label->borderLines & LEFT_SOLID)
+                display_line_draw(dsp, label->box.left, label->box.top,
+                    label->box.left,
+                    label->box.top + height, label->borderColor);
 
-			if (label->borderLines & RIGHT_SOLID)
-				display_line_draw(dsp, label->box.left + label->box.width, label->box.top,
-								  label->box.left + label->box.width,
-								  label->box.top + label->box.height, label->borderColor);
-		}
-		else
-		{ // Set all 4 Lines onto the 4 corners
+            if (label->borderLines & RIGHT_SOLID)
+                display_line_draw(dsp, label->box.left + width, label->box.top,
+                    label->box.left + width,
+                    label->box.top + height, label->borderColor);
+        } else { // Set all 4 Lines onto the 4 corners
 
-			/*  t1/l1 ------- t2/r1
-			 *    |             |
-			 *  b1/l2 ------- b2/r2
-			 */
+            /*  t1/l1 ------- t2/r1
+             *    |             |
+             *  b1/l2 ------- b2/r2
+             */
 
-			point_t t1 = {label->box.left, label->box.top};
-			point_t l1 = {label->box.left, label->box.top};
+            point_t t1 = { label->box.left, label->box.top };
+            point_t l1 = { label->box.left, label->box.top };
 
-			point_t t2 = {label->box.left + label->box.width, label->box.top};
-			point_t r1 = {label->box.left + label->box.width, label->box.top};
+            point_t t2 = { label->box.left + width, label->box.top };
+            point_t r1 = { label->box.left + width, label->box.top };
 
-			point_t b1 = {label->box.left, label->box.top + label->box.height};
-			point_t l2 = {label->box.left, label->box.top + label->box.height};
+            point_t l2 = { label->box.left, label->box.top + height };
+            point_t b1 = { label->box.left, label->box.top + height };
 
-			point_t b2 = {label->box.left + label->box.width, label->box.top + label->box.height};
-			point_t r2 = {label->box.left + label->box.width, label->box.top + label->box.height};
+            point_t b2 = { label->box.left + width, label->box.top + height };
+            point_t r2 = { label->box.left + width, label->box.top + height };
 
-			if (label->roundedCorners & TOP_LEFT)
-			{
-				display_circle_draw_segment(dsp, t1.left + label->roundedRadius,
-											t1.top + label->roundedRadius, label->roundedRadius,
-											label->borderColor, 192);
-				t1.left += label->roundedRadius;
-				l1.top += label->roundedRadius;
-			}
+            if (label->roundedCorners & TOP_LEFT) {
+                display_circle_draw_segment(dsp, t1.left + label->roundedRadius,
+                    t1.top + label->roundedRadius, label->roundedRadius,
+                    label->borderColor, 192);
+                t1.left += label->roundedRadius;
+                l1.top += label->roundedRadius;
+            }
 
-			if (label->roundedCorners & TOP_RIGHT)
-			{
-				display_circle_draw_segment(dsp, t2.left - label->roundedRadius,
-											t2.top + label->roundedRadius, label->roundedRadius,
-											label->borderColor, 3);
-				t2.left -= label->roundedRadius;
-				r1.top += label->roundedRadius;
-			}
+            if (label->roundedCorners & TOP_RIGHT) {
+                display_circle_draw_segment(dsp, t2.left - label->roundedRadius,
+                    t2.top + label->roundedRadius, label->roundedRadius,
+                    label->borderColor, 3);
+                t2.left -= label->roundedRadius;
+                r1.top += label->roundedRadius;
+            }
 
-			if (label->roundedCorners & BOTTOM_LEFT)
-			{
-				display_circle_draw_segment(dsp, b1.left + label->roundedRadius,
-											b1.top - label->roundedRadius, label->roundedRadius,
-											label->borderColor, 48);
-				b1.left += label->roundedRadius;
-				l2.top -= label->roundedRadius;
-			}
+            if (label->roundedCorners & BOTTOM_LEFT) {
+                display_circle_draw_segment(dsp, b1.left + label->roundedRadius,
+                    b1.top - label->roundedRadius, label->roundedRadius,
+                    label->borderColor, 48);
+                b1.left += label->roundedRadius;
+                l2.top -= label->roundedRadius;
+            }
 
-			if (label->roundedCorners & BOTTOM_RIGHT)
-			{
-				display_circle_draw_segment(dsp, b2.left - label->roundedRadius,
-											b2.top - label->roundedRadius, label->roundedRadius,
-											label->borderColor, 12);
-				b2.left -= label->roundedRadius;
-				r2.top -= label->roundedRadius;
-			}
+            if (label->roundedCorners & BOTTOM_RIGHT) {
+                display_circle_draw_segment(dsp, b2.left - label->roundedRadius,
+                    b2.top - label->roundedRadius, label->roundedRadius,
+                    label->borderColor, 12);
+                b2.left -= label->roundedRadius;
+                r2.top -= label->roundedRadius;
+            }
 
-			display_line_draw(dsp, t1.left, t1.top, t2.left, t2.top,
-							  label->borderColor);
-			display_line_draw(dsp, b1.left, b1.top, b2.left, b2.top,
-							  label->borderColor);
-			display_line_draw(dsp, l1.left, l1.top, l2.left, l2.top,
-							  label->borderColor);
-			display_line_draw(dsp, r1.left, r1.top, r2.left, r2.top,
-							  label->borderColor);
-		}
-	}
-	if (label->onAfterRender)
-		if (label->onAfterRender(dsp, label) != PM_OK)
-		{
-			return ABORT;
-		}
-	return PM_OK;
+            display_line_draw(dsp, t1.left, t1.top, t2.left, t2.top,
+                label->borderColor);
+            display_line_draw(dsp, b1.left, b1.top, b2.left, b2.top,
+                label->borderColor);
+            display_line_draw(dsp, l1.left, l1.top, l2.left, l2.top,
+                label->borderColor);
+            display_line_draw(dsp, r1.left, r1.top, r2.left, r2.top,
+                label->borderColor);
+        }
+    }
+    if (label->onAfterRender)
+        if (label->onAfterRender(dsp, label) != PM_OK) {
+            return ABORT;
+        }
+    return PM_OK;
 }
 
 /*
- * set label box size to font width and height to 
+ * set label box size to font width and height to
  * font hieht or image height if image is larger
  * than font.
  */
-error_code_t label_shrink_to_text(label_t *label)
+error_code_t label_shrink_to_text(label_t* label)
 {
-	label->box.width = font_text_pixel_width(label->font, label->text);
-	label->box.height = font_text_pixel_height(label->font, label->text);
-	if(label->child) {
-		image_t* img = label->child;
-		if(img->box.height > label->box.height)
-			label->box.height = img->box.height;
-	}
-	return PM_OK;
+    label->box.width = font_text_pixel_width(label->font, label->text);
+    label->box.height = font_text_pixel_height(label->font, label->text);
+    if (label->child) {
+        image_t* img = label->child;
+        if (img->box.height > label->box.height)
+            label->box.height = img->box.height;
+    }
+    return PM_OK;
 }
