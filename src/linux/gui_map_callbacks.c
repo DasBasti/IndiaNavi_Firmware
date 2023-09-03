48ef3edfc18ddc0017b839bb6ef06331863e8c8c
Bastian Neumann <neumann.bastian@gmail.com>
2023-09-03 19:07:44 +0200

    Linux: Play around with curl loading of map images

--- src/linux/gui_map_callbacks.c
+++ src/linux/gui_map_callbacks.c
@@ -9,11 +9,30 @@
 #include "gui/map.h"
 #include "tasks.h"
 
-#include <stdio.h>
 #include <fcntl.h>
+#include <stdio.h>
 #include <unistd.h>
 
+#if USE_CURL
+#    include <curl/curl.h>
+#    include <curl/easy.h>
+#endif
+
 static const char* TAG = "GUI_MAP";
+char* path_prefix = "tiles/raw";
+
+void set_path_prefix(char* prefix)
+{
+    path_prefix = prefix;
+}
+
+size_t load_data(void* ptr, size_t size, size_t nmemb, FILE* stream)
+{
+    image_t* img = (image_t*)stream;
+    ESP_LOGI(__func__, "Loaded: %lu start @ %p", size * nmemb, img->data);
+    memcpy(img->data, ptr, size * nmemb);
+    return size;
+}
 
 /*
  * Load tile data from SD Card on render command
@@ -30,35 +49,49 @@ error_code_t load_map_tile_on_demand(const display_t* dsp, void* image)
         return UNAVAILABLE;
 
     image_t* img = (image_t*)image;
+    label_t* l = (label_t*)img->child;
 
     img->data = imageBuf;
     map_tile_t* tile = img->parent; // the parent component of the image is the tile
-    
+
     uint32_t br;
     // TODO: decompress lz4 tiles
-    save_sprintf(fn, "tiles/raw/%u/%u/%u.raw",
+    save_sprintf(fn, "%s/%u/%u/%u.raw",
+        path_prefix,
         tile->z,
         tile->x,
         tile->y);
     ESP_LOGI(TAG, "Load %s  to %p", fn, tile->image->data);
 
-        fd = open(fn, O_RDONLY);
-        if (-1 != fd && tile->image->data != 0) {
-            ssize_t count;
-            count = read(fd, tile->image->data, 32768); // Tilesize 256*256/2 bytes
-            ESP_LOGI(TAG, "Load %ld bytes", count);
-            if (count == 32768) {
-                tile->image->loaded = LOADED;
-            }
-            close(fd);
-        } else {
-            tile->image->loaded = NOT_FOUND;
-        }
+#if USE_CURL
+    CURL* curl = curl_easy_init();
+    CURLcode res;
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, fn);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, load_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, tile->image);
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+    }
 
+#else
+    fd = open(fn, O_RDONLY);
+    if (-1 != fd && tile->image->data != 0) {
+        ssize_t count;
+        count = read(fd, tile->image->data, 32768); // Tilesize 256*256/2 bytes
+        ESP_LOGI(TAG, "Load %ld bytes", count);
+        if (count == 32768) {
+            tile->image->loaded = LOADED;
+            l->text = NULL;
+        }
+        close(fd);
+    } else {
+        tile->image->loaded = NOT_FOUND;
+    }
+#endif
     if (img->loaded == LOADED)
         return PM_OK;
 
-    label_t* l = (label_t*)img->child;
     if (img->loaded == ERROR) {
         l->text = "Error";
         goto freeImageMemory;
